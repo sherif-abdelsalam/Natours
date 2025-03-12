91c402e39af37e722e8a8003ab1d99fc0d0547b1
Sherif AbdleSalam <sherif.abdelsalam.111@gmail.com>
2025-03-12 22:04:31 +0200

    add tour statistics using mongoose aggreagation

--- natours/routers/tourRouter.js
+++ natours/routers/tourRouter.js
@@ -6,12 +6,16 @@ const router = express.Router();
 const tourController = require("../controllers/tourController");
 
 
-router.route("/top-5-cheap").get(tourController.aliasTopTours, tourController.getAllTours);
 
 router.route("/")
     .get(tourController.getAllTours)
     .post(tourController.createTour);
 
+router.route("/top-5-cheap").get(tourController.aliasTopTours, tourController.getAllTours);
+
+router.route("/tour-stats").get(tourController.getTourStats);
+
+router.route("/monthly-plan/:year").get(tourController.getMonthlyPlan);
 
 router.route("/:id")
     .get(tourController.getTour)
