1a3419ac0515de6a947163d9b848f054728b6d06
Sherif Abdelsalam <sherif.abdelsalam.111@gmail.com>
2025-09-07 21:53:39 +0300

    add overview pug template

--- natours/controllers/viewController.js
+++ natours/controllers/viewController.js
@@ -1,12 +1,29 @@
+const Tour = require("../models/tourModels");
+const catchAsync = require("../utils/catchAsync");
 
-exports.getOverview = (req, res) => {
+exports.getOverview = catchAsync(async(req, res) => {
+    // 1) get tour data from collection
+    // 2) build template
+    // 3) render that template using tour data from 1)
+
+    const tours = await Tour.find();
+
     res.status(200).render('overview', {
-        title: 'All Tours'
+        title: 'All Tours',
+        tours
     });
-};
+});
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async(req, res) => {
+    const {slug} = req.params;
+    // 1) get the data, for the requested tour (including reviews and guides)
+    // 2) build template
+    // 3) render template using data from 1)
+
+    const tour = await Tour.findOne({slug}).populate({path: 'reviews', select: 'review rating user'});
+
     res.status(200).render('tour', {
-        title: 'The Forest Hiker Tour'
+        title: tour.name,
+        tour
     });
-};
+});
--- natours/routers/viewRouter.js
+++ natours/routers/viewRouter.js
@@ -5,7 +5,7 @@ const viewController = require('../controllers/viewController');
 const router = Router();
 
 router.get('/', viewController.getOverview);
-router.get('/tour', viewController.getTour);
+router.get('/tour/:slug', viewController.getTour);
 // router.get('/login', viewController.getLoginForm);
 // router.get('/me', viewController.getAccount);
 // router.post('/submit-user-data', viewController.updateUserData);
