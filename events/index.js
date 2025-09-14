7ea8467f62d7ab715a943f7fff23f4853dabbcd2
Sherif Abdelsalam <sherif.abdelsalam.111@gmail.com>
2025-09-14 23:50:47 +0300

    remove un necessry files

--- events/index.js
+++
@@ -1,9 +0,0 @@
-const logEvent = require("./logEvent");
-const EventEmitter = require("events");
-
-class MyEmitter extends EventEmitter { };
-const myEmitter = new MyEmitter();
-
-myEmitter.on("log", (eventMessage) => logEvent(eventMessage));
-
-myEmitter.emit("log", "Event emitted");
--- node-js-intro/date.js
+++
@@ -1,7 +0,0 @@
-const { format } = require("date-fns");
-
-const now = new Date();
-
-console.log(format(now, "dd-MM-yyyy\tHH:mm:ss"));
-
-
