070caf2d55210140fb5fafeacfe5af615143c30c
Sherif Abdelsalam <sherif.abdelsalam.111@gmail.com>
2025-09-18 01:58:40 +0300

    modified checkouts

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -12,9 +12,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     mode: 'payment', // required now
-    success_url: `${req.protocol}://${req.get('host')}/?tour=${
-      req.params.tourId
-    }&user=${req.user.id}&price=${tour.price}`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
