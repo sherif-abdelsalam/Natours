23de7a1f5d3ce88d156c1878a72e6d8916088fd9
Sherif Abdelsalam <sherif.abdelsalam.111@gmail.com>
2025-09-18 01:59:26 +0300

    modified checkouts

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -50,7 +50,6 @@ exports.updateBooking = factory.updateOne(Booking);
 exports.deleteBooking = factory.deleteOne(Booking);
 
 const createBookingCheckout = catchAsync(async session => {
-  console.log(session);
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.amount_total / 100;
